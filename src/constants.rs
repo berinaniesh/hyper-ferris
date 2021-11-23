c097dc33697bfb3345f4671ba14efc53a1dc4e85
= <=>
2021-11-23 20:28:55 +0530

    move constants to separate file

---
+++ src/constants.rs
@@ -0,0 +1,83 @@
+pub const a8: i8 = 0;
+pub const b8: i8 = 1;
+pub const c8: i8 = 2;
+pub const d8: i8 = 3;
+pub const e8: i8 = 4;
+pub const f8: i8 = 5;
+pub const g8: i8 = 6;
+pub const h8: i8 = 7;
+pub const a7: i8 = 8;
+pub const b7: i8 = 9;
+pub const c7: i8 = 10;
+pub const d7: i8 = 11;
+pub const e7: i8 = 12;
+pub const f7: i8 = 13;
+pub const g7: i8 = 14;
+pub const h7: i8 = 15;
+pub const a6: i8 = 16;
+pub const b6: i8 = 17;
+pub const c6: i8 = 18;
+pub const d6: i8 = 19;
+pub const e6: i8 = 20;
+pub const f6: i8 = 21;
+pub const g6: i8 = 22;
+pub const h6: i8 = 23;
+pub const a5: i8 = 24;
+pub const b5: i8 = 25;
+pub const c5: i8 = 26;
+pub const d5: i8 = 27;
+pub const e5: i8 = 28;
+pub const f5: i8 = 29;
+pub const g5: i8 = 30;
+pub const h5: i8 = 31;
+pub const a4: i8 = 32;
+pub const b4: i8 = 33;
+pub const c4: i8 = 34;
+pub const d4: i8 = 35;
+pub const e4: i8 = 36;
+pub const f4: i8 = 37;
+pub const g4: i8 = 38;
+pub const h4: i8 = 39;
+pub const a3: i8 = 40;
+pub const b3: i8 = 41;
+pub const c3: i8 = 42;
+pub const d3: i8 = 43;
+pub const e3: i8 = 44;
+pub const f3: i8 = 45;
+pub const g3: i8 = 46;
+pub const h3: i8 = 47;
+pub const a2: i8 = 48;
+pub const b2: i8 = 49;
+pub const c2: i8 = 50;
+pub const d2: i8 = 51;
+pub const e2: i8 = 52;
+pub const f2: i8 = 53;
+pub const g2: i8 = 54;
+pub const h2: i8 = 55;
+pub const a1: i8 = 56;
+pub const b1: i8 = 57;
+pub const c1: i8 = 58;
+pub const d1: i8 = 59;
+pub const e1: i8 = 60;
+pub const f1: i8 = 61;
+pub const g1: i8 = 62;
+pub const h1: i8 = 63;
+
+pub enum CellNames {
+    A8, B8, C8, D8, E8, F8, G8, H8,
+    A7, B7, C7, D7, E7, F7, G7, H7,
+    A6, B6, C6, D6, E6, F6, G6, H6,
+    A5, B5, C5, D5, E5, F5, G5, H5,
+    A4, B4, C4, D4, E4, F4, G4, H4,
+    A3, B3, C3, D3, E3, F3, G3, H3,
+    A2, B2, C2, D2, E2, F2, G2, H2,
+    A1, B1, C1, D1, E1, F1, G1, H1,
+    }
+
+pub const WHITE: i8 = 0;
+pub const BLACK: i8 = 1;
+
+const NOT_A_FILE: u64 = 18374403900871474942;
+const NOT_AB_FILE: u64 = 18229723555195321596;
+const NOT_HG_FILE: u64 = 4557430888798830399;
+const NOT_H_FILE: u64 = 9187201950435737471;
--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,4 @@
-enum CellNames {
-A8, B8, C8, D8, E8, F8, G8, H8,
-A7, B7, C7, D7, E7, F7, G7, H7,
-A6, B6, C6, D6, E6, F6, G6, H6,
-A5, B5, C5, D5, E5, F5, G5, H5,
-A4, B4, C4, D4, E4, F4, G4, H4,
-A3, B3, C3, D3, E3, F3, G3, H3,
-A2, B2, C2, D2, E2, F2, G2, H2,
-A1, B1, C1, D1, E1, F1, G1, H1,
-}
-
-//=======================================================
-const a8: i8 = 0;
-const b8: i8 = 1;
-const c8: i8 = 2;
-const d8: i8 = 3;
-const e8: i8 = 4;
-const f8: i8 = 5;
-const g8: i8 = 6;
-const h8: i8 = 7;
-const a7: i8 = 8;
-const b7: i8 = 9;
-const c7: i8 = 10;
-const d7: i8 = 11;
-const e7: i8 = 12;
-const f7: i8 = 13;
-const g7: i8 = 14;
-const h7: i8 = 15;
-const a6: i8 = 16;
-const b6: i8 = 17;
-const c6: i8 = 18;
-const d6: i8 = 19;
-const e6: i8 = 20;
-const f6: i8 = 21;
-const g6: i8 = 22;
-const h6: i8 = 23;
-const a5: i8 = 24;
-const b5: i8 = 25;
-const c5: i8 = 26;
-const d5: i8 = 27;
-const e5: i8 = 28;
-const f5: i8 = 29;
-const g5: i8 = 30;
-const h5: i8 = 31;
-const a4: i8 = 32;
-const b4: i8 = 33;
-const c4: i8 = 34;
-const d4: i8 = 35;
-const e4: i8 = 36;
-const f4: i8 = 37;
-const g4: i8 = 38;
-const h4: i8 = 39;
-const a3: i8 = 40;
-const b3: i8 = 41;
-const c3: i8 = 42;
-const d3: i8 = 43;
-const e3: i8 = 44;
-const f3: i8 = 45;
-const g3: i8 = 46;
-const h3: i8 = 47;
-const a2: i8 = 48;
-const b2: i8 = 49;
-const c2: i8 = 50;
-const d2: i8 = 51;
-const e2: i8 = 52;
-const f2: i8 = 53;
-const g2: i8 = 54;
-const h2: i8 = 55;
-const a1: i8 = 56;
-const b1: i8 = 57;
-const c1: i8 = 58;
-const d1: i8 = 59;
-const e1: i8 = 60;
-const f1: i8 = 61;
-const g1: i8 = 62;
-const h1: i8 = 63;
-//===============================================================
-//Useful Constants
-
-//Side to move constant
-const WHITE: i8 = 0;
-const BLACK: i8 = 1;
-
-//Not specific file constants (for attack tables)
-const NOT_A_FILE: u64 = 18374403900871474942;
-const NOT_AB_FILE: u64 = 18229723555195321596;
-const NOT_HG_FILE: u64 = 4557430888798830399;
-const NOT_H_FILE: u64 = 9187201950435737471;
-
-//Attack Tables generation
+mod constants;
 
 fn mask_attack_squares (bitboard: &mut u64, square: u64, side: i8) {
     let attack: u64 = 0;
@@ -132,6 +43,7 @@ fn print_bitboard (bitboard: u64) {
 fn main() {
     println!("\n\n   Hyper Ferris 0.1.0\n");
     let mut bitboard: u64 = 0;
-    bitboard |= 1 << f4;
+    bitboard |= 1 << constants::f4;
     print_bitboard(bitboard);
+
 }
