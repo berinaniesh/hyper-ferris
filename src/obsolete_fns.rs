e3bb1f3c60c4009ffb8cbd3263d8d89ef6823a67
= <=>
2021-11-25 14:30:09 +0530

    backup obsolete fns

--- src/obsolete_fns.rs
+++ src/obsolete_fns.rs
@@ -13,3 +13,36 @@ fn set_king_attack_squares (square: u64) -> u64 {
     return attacks;
 }
 
+fn init_attack_tables (){
+    unsafe {
+        for side in 0..2 {
+            for square in 0..64 {
+                constants::PAWN_ATTACKS[side][square] = set_pawn_attack_squares(side, square as u64);
+                if side == 1 {
+                    constants::KNIGHT_ATTACKS[square] = set_knight_attack_squares(square as u64);
+                    constants::BISHOP_ATTACKS[square] = set_bishop_attack_squares(square as u64);
+                    constants::ROOK_ATTACKS[square] = set_rook_attack_squares(square as u64);
+                    constants::QUEEN_ATTACKS[square] = set_queen_attack_squares(square as u64);
+                    constants::KING_ATTACKS[square] = set_king_attack_squares(square as u64);
+                }    
+            }
+        }
+    }
+
+}
+
+fn set_pawn_attack_squares (side: usize, square: u64) -> u64 {
+    let mut attacks: u64 = 0;
+    let mut bitboard: u64 = 0;
+    set_bit(& mut bitboard, square);
+    if side == constants::WHITE {
+        if (constants::NOT_A_FILE & (bitboard >> 7)) > 0 {attacks |= bitboard >> 7};
+        if (constants::NOT_H_FILE & (bitboard >> 9)) > 0 {attacks |= bitboard >> 9};
+    }
+    else if side == constants::BLACK {
+        if (constants::NOT_A_FILE & (bitboard << 9)) > 0 {attacks |= bitboard << 9};
+         if (constants::NOT_H_FILE & (bitboard << 7)) > 0 {attacks |= bitboard << 7};
+    }
+    return attacks;
+}
+
