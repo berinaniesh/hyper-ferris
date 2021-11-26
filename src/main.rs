fb1848e326a1fe58ca04ac99d4261a176b9ae817
= <=>
2021-11-26 15:31:09 +0530

    hard code bishop attack tables

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,5 @@
 mod constants;
 
-fn set_bishop_attack_squares (square: i64) -> u64 {
-    let mut attacks = 0;
-    let tr = square/8;
-    let tf = square%8;
-    let mut rank;
-    let mut file;
-
-    rank = tr+1;
-    file = tf+1;
-    while rank < 7 && file < 7 {
-        attacks |= 1<<(rank*8 + file);
-        rank +=1;
-        file +=1;
-    }
-
-    rank = tr - 1;
-    file = tf + 1;
-    while rank > 0 && file < 7 {
-        attacks |= 1<<(rank *8 + file);
-        rank -= 1;
-        file += 1;
-    }
-
-    rank = tr + 1;
-    file = tf - 1;
-    while rank < 7 && file > 0 {
-        attacks |= 1<<(rank*8 + file);
-        rank += 1;
-        file -=1;
-    }
-
-    rank = tr-1;
-    file = tf-1;
-    while file > 0 && rank > 0 {
-        attacks |= 1<<(rank*8 + file);
-        rank -=1;
-        file -=1;
-    }
-
-    return attacks;
-}
-
 fn get_bit(bitboard: u64, square: u64) -> bool {
     return if bitboard & (1 << square) > 0 {true} else {false};
 }
@@ -69,10 +27,7 @@ fn print_bitboard (bitboard: u64) {
 
 fn main() {
     println!("\n\n   Hyper Ferris 0.1.0\n");
-    unsafe {
-        for square in 0..64 {
-            constants::BISHOP_ATTACKS[square] = set_bishop_attack_squares(square as i64);
-            println!("{},", constants::BISHOP_ATTACKS[square]);
-        }
+    for square in 0..64 {
+        print_bitboard(constants::BISHOP_ATTACKS[square]);
     }
 }
--- src/obsolete_fns.rs
+++ src/obsolete_fns.rs
@@ -61,3 +61,45 @@ fn set_knight_attack_squares (square: u64) -> u64 {
     if (constants::NOT_GH_FILE & (bitboard << 6)) > 0 {attacks |= bitboard << 6};
     return attacks;
 }
+
+fn set_bishop_attack_squares (square: i64) -> u64 {
+    let mut attacks = 0;
+    let tr = square/8;
+    let tf = square%8;
+    let mut rank;
+    let mut file;
+
+    rank = tr+1;
+    file = tf+1;
+    while rank < 7 && file < 7 {
+        attacks |= 1<<(rank*8 + file);
+        rank +=1;
+        file +=1;
+    }
+
+    rank = tr - 1;
+    file = tf + 1;
+    while rank > 0 && file < 7 {
+        attacks |= 1<<(rank *8 + file);
+        rank -= 1;
+        file += 1;
+    }
+
+    rank = tr + 1;
+    file = tf - 1;
+    while rank < 7 && file > 0 {
+        attacks |= 1<<(rank*8 + file);
+        rank += 1;
+        file -=1;
+    }
+
+    rank = tr-1;
+    file = tf-1;
+    while file > 0 && rank > 0 {
+        attacks |= 1<<(rank*8 + file);
+        rank -=1;
+        file -=1;
+    }
+
+    return attacks;
+}
