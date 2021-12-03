711b45fe2a74a00ddbc6a4eaeff892073fa04383
= <=>
2021-12-03 07:32:15 +0530

    magic bitboards prep

--- src/main.rs
+++ src/main.rs
@@ -88,6 +88,86 @@ fn magic_rook_attacks (square: i64, blocker: u64) -> u64 {
     return attacks;
 }
 
+fn set_rook_attack_squares (square: i64) -> u64 {
+    let mut attacks = 0;
+    let tr = square/8;
+    let tf = square%8;
+    let mut rank;
+    let mut file;
+
+    rank = tr+1;
+    file = tf;
+    while rank < 7 {
+        attacks |= 1<<(rank*8 + file);
+        rank +=1;
+    }
+
+    rank = tr - 1;
+    file = tf;
+    while rank > 0 {
+        attacks |= 1<<(rank*8 + file);
+        rank -= 1;
+    }
+
+    rank = tr;
+    file = tf - 1;
+    while file > 0 {
+        attacks |= 1<<(rank*8 + file);
+        file -=1;
+    }
+
+    rank = tr;
+    file = tf + 1;
+    while file < 7 {
+        attacks |= 1<<(rank*8 + file);
+        file += 1;
+    }
+
+    return attacks;
+}
+
+fn set_bishop_attack_squares (square: i64) -> u64 {
+    let mut attacks = 0;
+    let tr = square/8;
+    let tf = square%8;
+    let mut rank;
+    let mut file;
+
+    rank = tr+1;
+    file = tf+1;
+    while rank < 7 && file < 7 {
+        attacks |= 1<<(rank*8 + file);
+        rank +=1;
+        file +=1;
+    }
+
+    rank = tr - 1;
+    file = tf + 1;
+    while rank > 0 && file < 7 {
+        attacks |= 1<<(rank *8 + file);
+        rank -= 1;
+        file += 1;
+    }
+
+    rank = tr + 1;
+    file = tf - 1;
+    while rank < 7 && file > 0 {
+        attacks |= 1<<(rank*8 + file);
+        rank += 1;
+        file -=1;
+    }
+
+    rank = tr-1;
+    file = tf-1;
+    while file > 0 && rank > 0 {
+        attacks |= 1<<(rank*8 + file);
+        rank -=1;
+        file -=1;
+    }
+
+    return attacks;
+}
+
 fn get_bit(bitboard: u64, square: u64) -> bool {
     return if bitboard & (1 << square) > 0 {true} else {false};
 }
@@ -101,6 +181,7 @@ fn pop_bit(bitboard: &mut u64, square: u64) {
 }
 
 fn pop_least_significant_bit(bitboard: &mut u64) {
+    //Don't run it with bitboard = 0, thread panics due to underflow. 
         if *bitboard > 0 {*bitboard &= *bitboard - 1;}
 }
 
@@ -114,7 +195,7 @@ fn count_no_of_set_bits (mut bitboard: u64) -> u8 {
 }
 
 fn find_least_significant_bit (bitboard: u64) -> usize {
-    //Take care not to run this fn with bitboard = 0, it gives a wrong answer.
+    //Don't run this fn with bitboard = 0, gives a wrong answer.
     let mut count: usize = 0;
     let mut number: u64 = 1;
     if bitboard == 0 {return 0};
@@ -123,6 +204,18 @@ fn find_least_significant_bit (bitboard: u64) -> usize {
         number = number << 1;
     }
     return count;
+}
+
+fn set_occupancy(index: i32, bits_in_mask: i32, mut attack_mask: u64) -> u64 {
+    let mut occupancy = 0u64;
+    for count in 0..bits_in_mask {
+        let square = find_least_significant_bit(attack_mask);
+        pop_bit (&mut attack_mask, square as u64);
+        if (index & (1<<count)) > 0 {
+            occupancy |= 1 << square;
+        }
+    }
+    return occupancy;
 }
 
 fn print_bitboard (bitboard: u64) {
@@ -141,6 +234,11 @@ fn print_bitboard (bitboard: u64) {
 
 fn main() {
     println!("\n\n   Hyper Ferris 0.1.0\n");
-    let mut bitboard: u64 = 0;
-    set_bit(&mut bitboard, constants::d4 as u64);
+    for rank in 0..8 {
+        for file in 0..8 {
+            let square = rank*8 + file;
+            print!("{}, ", count_no_of_set_bits(set_rook_attack_squares(square)));
+        }
+        println!();
+    }
 }
