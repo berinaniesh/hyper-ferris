7e716b427082472510df0089e39e517644bc6a36
= <=>
2021-12-21 12:11:17 +0530

    update

---
+++ src/board.rs
@@ -0,0 +1,19 @@
+struct Board {
+    white_pawns: u64;
+    black_pawns: u64;
+    white_knights: u64;
+    black_knights: u64;
+    white_bishops: u64;
+    black_bishops: u64;
+    white_rooks: u64;
+    black_rooks:u64;
+    white_queens: u64;
+    black_queens: u64;
+    white_king: u64;
+    black_king: u64;
+}
+impl Board {
+    fn reset board () {
+        
+    }
+}
